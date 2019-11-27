9c3fa3a16b8a25813e276f6376a5531253ea2144
Brian Chambers <brian@brianchambers.dev>
2019-11-27 09:09:23 -0600

    Changed string from 'Hello World!' to 'Hello Blog!'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloBlog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Blog!");
         }
     }
 }
